b6df6af4d05ca2e0b6029b42dc1773a4a3c9537c
Mark Platonov <37633544+monstror@users.noreply.github.com>
2021-04-28 21:21:43 +0700

    fixed incorrect rounding
    replaced "round" with "ceil"

--- magnit.py
+++ magnit.py
@@ -1,3 +1,6 @@
+from numpy import ceil
+
+
 def shop_price_edit(goods: dict) -> dict:
     """Расчет стоимости товара с учетом 15% прибыли"""
-    return {name: int(round((price+(price*0.15)) / 5) * 5) for name, price in goods.items()}
+    return {name: int(ceil((price + (price * 0.15)) / 5) * 5) for name, price in goods.items()}
